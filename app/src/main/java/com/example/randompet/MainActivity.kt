780ceedd9701343f17cf1f9491d53b7ce0e2c82c
jemimagayy <jgay2022@fau.edu>
2025-07-14 17:41:36 -0400

    version 2.0

--- app/src/main/java/com/example/randompet/MainActivity.kt
+++ app/src/main/java/com/example/randompet/MainActivity.kt
@@ -14,40 +14,32 @@ import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
-
-    val button = findViewById<Button>(R.id.petButton)
-    val imageView = findViewById<ImageView>(R.id.petImage)
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.petButton)
+
+        val petButton = null
+        petButton?.let { setupButton(it) }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        fetchDogImage()
-
-        button.setOnClickListener {
-            fetchDogImage()
-        }
     }
 
-    private fun fetchDogImage() {
+    private fun fetchDogImage(imageview: ImageView) {
         val client = AsyncHttpClient()
+
         client["https://dog.ceo/api/breeds/image/random", object : JsonHttpResponseHandler() {
-
-            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
-
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+                val imageView = findViewById<ImageView>(R.id.petImage)
                 val petImageURL = json.jsonObject.getString("message")
                 Log.d("petImageURL", "pet image URL set: $petImageURL")
                 Log.d("Dog", "response successful: $json")
 
-                Glide.with(this@MainActivity)
-                    .load(petImageURL)
-                    .fitCenter()
-                    .into(imageView)
+                Glide.with(this@MainActivity).load(petImageURL).fitCenter().into(imageView)
             }
 
             override fun onFailure(
@@ -59,6 +51,16 @@ class MainActivity : AppCompatActivity() {
                 Log.d("Dog Error", errorResponse)
             }
         }]
+    }
+
+    private fun setupButton(button:Button) {
+        val imageView = findViewById<ImageView>(R.id.petImage)
+        val button = findViewById<Button>(R.id.petButton)
+        button.setOnClickListener {
+            fetchDogImage(
+                imageview = TODO()
+            )
+        }
     }
 
 }
